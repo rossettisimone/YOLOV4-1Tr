fixes in dataloader, added separate datasets for validation and train, added check on not finite numbers in labels, added gradient clip

loader.py
@@ -27,44 +27,71 @@ from utils import file_reader, mask_clamp, read_image, encode_target
 
 class Generator(object):
             
-    def _single_input_generator(self, index):
-        [video, frame_id] = self.annotation[index]
+    def _single_input_generator_train(self, index):
+        [video, frame_id] = self.annotation_train[index]
         image, masks, bboxes = self.data_generator(video, frame_id)
         if self.data_aug:
             image, masks, bboxes = self.data_augment(image, masks, bboxes)
-        image, masks, bboxes = self.data_resize(image, bboxes, masks, (self.train_input_size,self.train_input_size))
-        label_2, labe_3, label_4, label_5 = self.data_pyramid_lables(bboxes)
-        masks = self.masks_crop_resize(masks, bboxes, cfg.MASK_SIZE)
-        masks, bboxes = self.data_pad_max_instances(masks, bboxes, cfg.MAX_INSTANCES)
-        return image, label_2, labe_3, label_4, label_5, masks, bboxes
+        image, masks, bboxes = self.data_preprocess(image, bboxes, masks)
+        label_2, label_3, label_4, label_5 = self.data_labels(bboxes)
+        masks = self.masks_preprocess(masks, bboxes)
+        masks, bboxes = self.data_pad(masks, bboxes)
+        inputs = [image, label_2, label_3, label_4, label_5, masks, bboxes]
+        try:
+            for input_ in inputs:
+                assert np.isfinite(input_).all(), 'not finite values in inputs'
+        except:
+            for index, input_ in enumerate(inputs):
+                inputs[index] = np.zeros_like(input_)
+        return inputs
     
-    def data_pad_max_instances(self, masks, bboxes, max_instances):
-        bboxes_padded = np.zeros((max_instances,5))
+    def _single_input_generator_val(self, index):
+        image, masks, bboxes = self.data_generator(video, frame_id)
+        image, masks, bboxes = self.data_preprocess(image, bboxes, masks)
+        label_2, label_3, label_4, label_5 = self.data_labels(bboxes)
+        masks = self.masks_preprocess(masks, bboxes)
+        masks, bboxes = self.data_pad(masks, bboxes)
+        inputs = [image, label_2, label_3, label_4, label_5, masks, bboxes]
+        try:
+            for input_ in inputs:
+                assert np.isfinite(input_).all(), 'not finite values in inputs'
+        except:
+            for index, input_ in enumerate(inputs):
+                inputs[index] = np.zeros_like(input_)
+        return inputs
+    
+    def data_pad(self, masks, bboxes):
+        bboxes_padded = np.zeros(( cfg.MAX_INSTANCES,5))
         bboxes_padded[:,4]=-1
-        #check consistency of bbox after data augmentation
-        masks = masks[bboxes[...,2]>bboxes[...,0]]
-        masks = masks[bboxes[...,3]>bboxes[...,1]]
-        bboxes = bboxes[bboxes[...,2]>bboxes[...,0]]
-        bboxes = bboxes[bboxes[...,3]>bboxes[...,1]]
+        masks_padded = np.zeros(( cfg.MAX_INSTANCES,masks.shape[1],masks.shape[2]))
+        #check consistency of bbox after data augmentation: dimension and ratio
+        width = bboxes[...,2] - bboxes[...,0]
+        height = bboxes[...,3] - bboxes[...,1]
+        mask = (width > cfg.MIN_BOX_DIM) \
+            * (height > cfg.MIN_BOX_DIM) \
+            * ((width/height)>cfg.MIN_BOX_RATIO) \
+            * ((height/width)>cfg.MIN_BOX_RATIO)
+        bboxes = bboxes[mask]
+        masks = masks[mask]
         #zero pad
-        min_bbox = min(bboxes.shape[0], max_instances)
-        bboxes_padded[:min_bbox,:]=bboxes[:min_bbox,:]
-        masks_padded = np.zeros((max_instances,masks.shape[1],masks.shape[2]))
-        min_mask = min(masks.shape[0], max_instances)
-        masks_padded[:min_mask,:,:]=masks[:min_mask,:,:]
+        min_bbox = min(bboxes.shape[0],  cfg.MAX_INSTANCES)
+        bboxes_padded[:min_bbox,...]=bboxes[:min_bbox,...]
+        min_mask = min(masks.shape[0],  cfg.MAX_INSTANCES)
+        masks_padded[:min_mask,...]=masks[:min_mask,...]
         return masks_padded, bboxes_padded
         
-    def masks_crop_resize(self, masks, bboxes, mask_size):
-        masks_c_r = []
+    def masks_preprocess(self, masks, bboxes):
+        masks_resized = []
         for mask, bbox in zip(masks, bboxes):
             mask = Image.fromarray(mask[bbox[1]:bbox[3],bbox[0]:bbox[2]])
-            mask = np.round(mask.resize((mask_size,mask_size), Image.ANTIALIAS))
-            masks_c_r.append(mask)
-        masks = np.stack(masks_c_r,axis=0)
-        return masks
+            mask = np.round(mask.resize((cfg.MASK_SIZE,cfg.MASK_SIZE), Image.ANTIALIAS))
+            mask = np.clip(mask,0,1)
+            masks_resized.append(mask)
+        masks_resized = np.stack(masks_resized,axis=0)
+        return masks_resized
         
-    def data_resize(self, image, gt_boxes, masks, target_size):
-        ih, iw    = target_size
+    def data_preprocess(self, image, gt_boxes, masks):
+        ih, iw    = (cfg.TRAIN_SIZE, cfg.TRAIN_SIZE)
         h,  w, _  = image.shape
         scale = min(iw/w, ih/h)
         nw, nh  = int(scale * w), int(scale * h)    
@@ -74,6 +101,7 @@ class Generator(object):
         dw, dh = (iw - nw) // 2, (ih-nh) // 2
         image_paded[dh:nh+dh, dw:nw+dw, :] = image_resized
         image_paded = image_paded / 255.
+        image_paded=np.clip(image_paded,0,1)
         gt_boxes[:, [0, 2]] = gt_boxes[:, [0, 2]] * scale + dw
         gt_boxes[:, [1, 3]] = gt_boxes[:, [1, 3]] * scale + dh
         masks_padded = np.zeros((masks.shape[0],iw, ih))
@@ -87,32 +115,44 @@ class Generator(object):
     def data_generator(self, video, frame_id):
         frame_name = video['f_l'][frame_id]
         v_id =  video['v_id']
-#        class_ = 1
         path = os.path.join(cfg.VIDEOS_DATASET_PATH, v_id, frame_name+'.png' )
-        try:
-            image = np.array(read_image(path))
-        except:
-            image = np.array(Image.new('RGB', (cfg.TRAIN_SIZE, cfg.TRAIN_SIZE), color = (0, 0, 0)))
-        height, width, _ = image.shape
         bboxes = []
         masks = [] 
-        for person in video['p_l']:
-            box = person["bb_l"][frame_id]
-            box=np.clip(box,0,1)
-            if len(box)==8: # Siammask returns 8 scalars (rotated bbox, rectify them)
-                xx, yy = [s for i,s in enumerate(box) if i%2==0 ], [s for i,s in enumerate(box) if not i%2==0]
-                box=np.array([min(xx),min(yy),max(xx),max(yy)])
-            if not np.all(box==0) and box[2]>box[0] and box[3]>box[1]:
-                box = np.r_[box,1]*np.array([width, height, width, height, person['p_id']])
-                
+        try:
+            image = np.array(read_image(path))
+            # assert np.isfinite(image).all(), 'nan values in image'
+            height, width, _ = image.shape
+            for person in video['p_l']:
                 try:
-                     mask = mask_clamp(np.array(read_image(os.path.join(cfg.SEGMENTS_DATASET_PATH, v_id, frame_name+'_'+str(person['p_id'])+'.png' ))))
+                    box = person["bb_l"][frame_id]
+                    # assert np.isfinite(box).all(), 'nan values in box'
+                    if len(box)==8: # Siammask returns 8 scalars (rotated bbox, rectify them)
+                        xx, yy = [s for i,s in enumerate(box) if i%2==0 ], [s for i,s in enumerate(box) if not i%2==0]
+                        box=np.array([min(xx),min(yy),max(xx),max(yy)])
+                    box=np.clip(box,0,1)
+                    try:
+                        p_id = person['p_id']
+                        # assert np.isfinite(p_id), 'nan values in p_id'
+                    except:
+                        p_id = 0
+                    box = np.r_[box,1]*np.array([width, height, width, height, p_id]) 
+                    bboxes.append(box)
+                    try:
+                        path_mask = os.path.join(cfg.SEGMENTS_DATASET_PATH, v_id, frame_name+'_'+str(person['p_id'])+'.png' )
+                        mask = mask_clamp(np.array(read_image(path_mask)))
+                        # assert np.isfinite(mask).all(), 'nan values in mask'
+                    except:
+                        mask = np.array(Image.new('L', (width, height), color=0))
+                    masks.append(mask)
                 except:
-                     mask = np.array(Image.new('L', (width, height), color=0))
-                bboxes.append(box)
-                masks.append(mask)
-        bboxes = np.stack(bboxes,axis=0)
-        masks = np.stack(masks,axis=0)
+                    bboxes.append(np.zeros((1,5), dtype=np.float32))
+                    masks.append(np.zeros((width, height), dtype=np.float32))
+            bboxes = np.stack(bboxes,axis=0)
+            masks = np.stack(masks,axis=0)
+        except:
+            image = np.array(Image.new('RGB', (cfg.TRAIN_SIZE, cfg.TRAIN_SIZE), color = (0, 0, 0)))
+            bboxes = np.zeros((cfg.MAX_INSTANCES,5), dtype=np.float32) # bbox + pid
+            masks = np.zeros((cfg.MAX_INSTANCES, cfg.TRAIN_SIZE, cfg.TRAIN_SIZE), dtype=np.float32)
         return image, masks, bboxes
 
     def data_augment(self, image, masks, bboxes):
@@ -121,7 +161,7 @@ class Generator(object):
         image, masks, bboxes = self.random_translate(image, masks, bboxes)
         return image, masks, bboxes
     
-    def data_pyramid_lables(self, bboxes):
+    def data_labels(self, bboxes):
         labels = []
         for level in range(cfg.LEVELS):
             label = encode_target(bboxes, self.anchors[level]/self.strides[level], cfg.NUM_ANCHORS, self.num_classes, self.train_output_sizes[level], self.train_output_sizes[level])
@@ -199,42 +239,40 @@ class Generator(object):
 class DataLoader(Generator):
     def __init__(self, shuffle=True, data_aug=True):
         print('Dataset loading..')
-        self.shuffle=shuffle
+        self.shuffle = shuffle
         self.data_aug = data_aug
-        self.json_dataset = file_reader(cfg.ANNOTATION_PATH)
-        self.nID = 0        
-        if cfg.DATASET_TYPE == 'kinetics':
-            for i,k in enumerate(self.json_dataset):
-                for j,_ in enumerate(k['p_l']):
-                    # self.json_dataset[i]['p_l'][j]['p_id_2'] = self.nID
-                    self.nID += 1
-        elif cfg.DATASET_TYPE == 'ava':
-            self.max_id_in_video = {}
-            for i,k in enumerate(self.json_dataset):
-                for j,_ in enumerate(k['p_l']):
-                    try:
-                        if self.json_dataset[i]['p_l'][j]['p_id'] > self.max_id_in_video[self.json_dataset[i]['v_id']]:
-                            self.max_id_in_video[self.json_dataset[i]['v_id']] = self.json_dataset[i]['p_l'][j]['p_id'] 
-                    except KeyError as e:
-                        self.max_id_in_video[self.json_dataset[i]['v_id']] = 0
-        #     # keys are ordered
-        #     self.keys_offset = {}
-        #     self.keys = sorted(self.max_id_in_video.keys())
-        #     self.offset = [1 if i == 0 else sum([self.max_id_in_video[m] for j,m in enumerate(self.keys) if j<i]) + 1 + i for i,k in enumerate(self.keys)]
-        #     for i,k in enumerate(self.keys):
-        #         self.keys_offset[k] = self.offset[i]
-        #     for i,k in enumerate(self.json_dataset):
-        #         for j,_ in enumerate(k['p_l']):
-        #             self.json_dataset[i]['p_l'][j]['p_id_2'] = self.json_dataset[i]['p_l'][j]['p_id'] #+ self.keys_offset[self.json_dataset[i]['v_id']]
-            self.nID = sum(self.max_id_in_video.values()) + len(self.max_id_in_video.values()) # id starts from zero, count them
-        #     self.nID = max(self.max_id_in_video.values()) + 1 #* len(self.max_id_in_video.values()) + 1
-        self.annotation = [(video,frame_id) for video in self.json_dataset for frame_id in range(0,61) if not all(p['bb_l'][frame_id]==[0,0,0,0] for p in video['p_l'])] # (video,0),(video,10),..,(video,60) sample each 10 frames
-        self.train_list, self.val_list = self.split_dataset(len(self.annotation))
-        self.train_ds = self.initilize_ds(self.train_list)
-        self.val_ds = self.initilize_ds(self.val_list)
+        self.json_train_dataset = []
+        for file_path in cfg.TRAIN_ANNOTATION_PATH:
+            self.json_train_dataset += file_reader(file_path)
+            print('Train Dataset {} loaded'.format(file_path))
+        self.json_val_dataset = []
+        for file_path in cfg.VAL_ANNOTATION_PATH:
+            self.json_val_dataset += file_reader(file_path)
+            print('Validation Dataset {} loaded'.format(file_path))
+        max_id_in_video = {}
+        for i,k in enumerate(self.json_train_dataset):
+            for j,_ in enumerate(k['p_l']):
+                try:
+                    if self.json_train_dataset[i]['p_l'][j]['p_id'] > max_id_in_video[self.json_train_dataset[i]['v_id']]:
+                        max_id_in_video[self.json_train_dataset[i]['v_id']] = self.json_train_dataset[i]['p_l'][j]['p_id'] 
+                except KeyError as e:
+                    max_id_in_video[self.json_train_dataset[i]['v_id']] = 0
+        self.nID = sum(max_id_in_video.values()) + len(max_id_in_video.values()) # id starts from zero, count them
+        #self.annotation = [(video,frame_id) for video in self.json_train_dataset for frame_id in range(0,61) if not all(p['bb_l'][frame_id]==[0,0,0,0] for p in video['p_l'])] # (video,0),(video,10),..,(video,60) sample each 10 frames
+        #self.train_list, self.val_list = self.split_dataset(len(self.annotation_train))
+        self.annotation_train = [(video,frame_id) for video in self.json_train_dataset \
+                for frame_id in range(0,61) if not all(sum(p['bb_l'][frame_id])==0 for p in video['p_l'])]
+        self.annotation_val = [(video,frame_id) for video in self.json_val_dataset \
+                for frame_id in range(0,61) if not all(sum(p['bb_l'][frame_id])==0 for p in video['p_l'])]
+        self.train_list = np.arange(len(self.annotation_train))
+        self.val_list = np.arange(len(self.annotation_val))
+        if self.shuffle:
+            np.random.shuffle(self.train_list)
+            np.random.shuffle(self.val_list)
+        self.train_ds = self.initilize_train_ds(self.train_list)
+        self.val_ds = self.initilize_val_ds(self.val_list)
         self.num_classes = cfg.NUM_CLASS
         self.anchors = tf.reshape(tf.constant(cfg.ANCHORS,dtype=tf.float32),[cfg.LEVELS, cfg.NUM_ANCHORS, 2])
-#        self.anchor_per_scale = cfg.ANCHOR_PER_SCALE
         self.train_input_size = cfg.TRAIN_SIZE
         self.strides = tf.cast(cfg.STRIDES,tf.float32)
         self.train_output_sizes = self.train_input_size // self.strides
@@ -254,13 +292,22 @@ class DataLoader(Generator):
         for idx in range(len(id_list)):
             yield id_list[idx]
     
-    def read_transform(self, idx):
-        image, label_2, labe_3, label_4, label_5, masks, bboxes = tf.py_function(self._single_input_generator, [idx], [tf.float32, tf.float32, tf.float32, tf.float32, tf.float32, tf.float32, tf.float32])
-        return image, label_2, labe_3, label_4, label_5, masks, bboxes
+    def read_transform_train(self, idx):
+        image, label_2, label_3, label_4, label_5, masks, bboxes = tf.py_function(self._single_input_generator_train, [idx], [tf.float32, tf.float32, tf.float32, tf.float32, tf.float32, tf.float32, tf.float32])
+        return image, label_2, label_3, label_4, label_5, masks, bboxes
 
-    
-    def initilize_ds(self, list_ids):
+    def read_transform_val(self, idx):
+        image, label_2, label_3, label_4, label_5, masks, bboxes = tf.py_function(self._single_input_generator_val, [idx], [tf.float32, tf.float32, tf.float32, tf.float32, tf.float32, tf.float32, tf.float32])
+        return image, label_2, label_3, label_4, label_5, masks, bboxes
+
+    def initilize_train_ds(self, list_ids):
         ds = tf.data.Dataset.from_generator(DataLoader.input_generator , args= [list_ids], output_types= (tf.int32))
-        ds = ds.map(self.read_transform, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        ds = ds.map(self.read_transform_train, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        ds = ds.prefetch(tf.data.experimental.AUTOTUNE)
+        return ds
+    
+    def initilize_val_ds(self, list_ids):
+        ds = tf.data.Dataset.from_generator(DataLoader.input_generator , args= [list_ids], output_types= (tf.int32))
+        ds = ds.map(self.read_transform_val, num_parallel_calls=tf.data.experimental.AUTOTUNE)
         ds = ds.prefetch(tf.data.experimental.AUTOTUNE)
         return ds

models.py
@@ -78,13 +78,11 @@ class MSDS(tf.keras.Model): #MSDS, multi subject detection and segmentation
         self.emb = emb
         if data_loader is not None:
             self.ds = data_loader
-            self.train_ds = self.ds.train_ds
-            self.val_ds = self.ds.val_ds
             self.nID =  self.ds.nID
             self.epochs = cfg.EPOCHS
-            self.epoch = 0
-            self.step_train = 0
-            self.step_val = 0
+            self.epoch = 1
+            self.step_train = 1
+            self.step_val = 1
             self.batch = cfg.BATCH
             self.steps_train = cfg.STEPS_PER_EPOCH_TRAIN
             self.steps_val = cfg.STEPS_PER_EPOCH_VAL
@@ -98,7 +96,8 @@ class MSDS(tf.keras.Model): #MSDS, multi subject detection and segmentation
         self.STRIDES = tf.constant(cfg.STRIDES,dtype=tf.float32)
         self.emb_dim = cfg.EMB_DIM 
         self.emb_scale = (tf.math.sqrt(2.0) * tf.math.log(self.nID-1.0)) if self.nID>1.0 else 1.0
-        self.optimizer = tfa.optimizers.SGDW( weight_decay = cfg.WD, learning_rate = cfg.LR, momentum = cfg.MOM, nesterov = False) #tf.keras.optimizers.Adam(learning_rate = cfg.LR)#
+        self.optimizer = tfa.optimizers.SGDW( weight_decay = cfg.WD, learning_rate = cfg.LR, momentum = cfg.MOM, nesterov = False) #tf.keras.optimizers.Adam(learning_rate = cfg.LR)
+        self.optimizer.clipvalue = cfg.GRADIENT_CLIP
 #        self.strategy = tf.distribute.MirroredStrategy()
 
         #CSPDARKNET53 
@@ -266,8 +265,9 @@ class MSDS(tf.keras.Model): #MSDS, multi subject detection and segmentation
         if epochs is not None:
             self.epochs = epochs
         self.start_epoch = start_epoch
-        for epoch in range(self.epochs):
-            self.epoch += 1
+        train_generator = self.ds.train_ds.repeat().batch(self.batch).__iter__()
+        val_generator = self.ds.val_ds.repeat().batch(self.batch).__iter__()
+        while self.epoch < self.epochs:
             if self.freeze_bkbn and self.epoch < 2:
                 self.bkbn.trainable = False
             else:
@@ -276,23 +276,26 @@ class MSDS(tf.keras.Model): #MSDS, multi subject detection and segmentation
                 self.freeze_batch_normalization() 
             # self.adapt_lr()
             self.set_trainable(trainable = True, include_backbone = False)
-            for data in self.train_ds.take(self.steps_train * self.batch).batch(self.batch):
-                self.step_train += 1
+            while self.step_train < self.epoch * self.steps_train :
+                data = train_generator.next()
                 losses = self.train_step(data)
                 self.loss_summary(losses, training=True)
                 self.print_loss(losses, training=True)
+                self.step_train += 1
             self.set_trainable(trainable = False, include_backbone = True)
-            gc.collect()
             mean_loss = []
-            for data in self.val_ds.take(self.steps_val * self.batch).batch(self.batch):
-                self.step_val += 1
+            gc.collect()
+            while self.step_val < self.epoch * self.steps_val :
+                data = val_generator.next()
                 losses = self.test_step(data) 
                 mean_loss.append(losses[0])
                 self.loss_summary(losses, training=False)
                 self.print_loss(losses, training=False)
+                self.step_val += 1
             path = "./{}/weights/{}_{}_{}_{}_{:0.5f}_{}.tf".format(self.folder,self.name,'emb' if self.emb else 'noemb','mask' if self.mask else 'nomask',self.epoch, tf.reduce_mean(mean_loss),datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
             self.save(path)
             gc.collect()
+            self.epoch += 1
     
 #    @tf.function
     def infer(self, image):