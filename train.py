e36f2af35f142bde54bbb7e8bf824d6dae141f72
SimoneRosset <rossetti.1900592@studenti.uniroma1.it>
2021-03-23 19:59:54 +0100

    SOFTMAX CROSS ENTROPY LOSS FOR MASK

--- train.py
+++ train.py
@@ -51,7 +51,7 @@ with strategy.scope():
 train_history = model.fit(dataset.train_ds, epochs = cfg.FINE_TUNING, steps_per_epoch = cfg.STEPS_PER_EPOCH_TRAIN, \
                       validation_data = dataset.val_ds, validation_steps = cfg.STEPS_PER_EPOCH_VAL,\
                       validation_freq = 1, max_queue_size = GLOBAL_BATCH * 10,
-                      callbacks = [callbacks, checkpoint], use_multiprocessing = True, workers = 48)
+                      callbacks = [callbacks, checkpoint], use_multiprocessing = True, workers = 24)
 
 model.evaluate(val_dataset, batch_size = GLOBAL_BATCH, callbacks = [callbacks], steps = cfg.STEPS_PER_EPOCH_VAL)
 
